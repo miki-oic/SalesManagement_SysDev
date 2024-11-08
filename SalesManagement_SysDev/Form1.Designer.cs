7876aec1c7de0093e6e6ff5df86026fd478393c2
miki <miki@oic.jp>
2024-11-08 11:58:23 +0900

    条件検索を実装

--- SalesManagement_SysDev/Form1.Designer.cs
+++ SalesManagement_SysDev/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             panel1 = new Panel();
             dataGridView1 = new DataGridView();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label2 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -52,22 +57,76 @@
             dataGridView1.Size = new Size(646, 226);
             dataGridView1.TabIndex = 0;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(79, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(147, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(558, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(167, 23);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(147, 79);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(79, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
             Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
         private DataGridView dataGridView1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label2;
     }
 }
--- SalesManagement_SysDev/Form1.cs
+++ SalesManagement_SysDev/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace SalesManagement_SysDev
 {
@@ -27,14 +28,58 @@ namespace SalesManagement_SysDev
         private void Initialize()
         {
 
-            SalesManagementContext context = new SalesManagementContext();
-
-            IDbContextTransaction dbContextTransaction = context.Database.BeginTransaction();
-            
-            dataGridView1.DataSource = context.MMakers.ToList();
+            label1.Text = "MaId";
+            label2.Text = "MaName";
+            button1.Text = "指定内容で検索する";
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SalesManagementContext context = new SalesManagementContext();
+
+            IDbContextTransaction dbContextTransaction = context.Database.BeginTransaction();
+
+            if (textBox1.Text.Length > 0 && textBox2.Text.Length > 0)
+            {
+
+                dataGridView1.DataSource = (
+                    from mMaker in context.MMakers
+                    where mMaker.MaId == int.Parse(textBox1.Text)
+                    where mMaker.MaName.StartsWith(textBox2.Text)
+                    select mMaker
+                    ).ToList();
+
+            }
+            else if(textBox1.Text.Length > 0)
+            {
+
+                dataGridView1.DataSource = (
+                    from mMaker in context.MMakers
+                    where mMaker.MaId == int.Parse(textBox1.Text)
+                    select mMaker
+                    ).ToList();
+
+            }
+            else if (textBox2.Text.Length > 0)
+            {
+
+                dataGridView1.DataSource = (
+                    from mMaker in context.MMakers
+                    where mMaker.MaName.StartsWith(textBox2.Text)
+                    select mMaker
+                    ).ToList();
+
+            }
+            else
+            {
+
+                dataGridView1.DataSource = context.MMakers.ToList();
+
+            }
+
+        }
     }
 
 }
