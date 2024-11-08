035ae08204644444c5a3c125fff38e5eb6e0f8b3
miki <miki@oic.jp>
2024-11-08 12:04:12 +0900

    参考サイト情報を説明に追加

--- SalesManagement_SysDev/Form1.cs
+++ SalesManagement_SysDev/Form1.cs
@@ -34,6 +34,12 @@ namespace SalesManagement_SysDev
 
         }
 
+        /// <summary>
+        /// 参考サイト：
+        /// https://learn.microsoft.com/ja-jp/ef/ef6/querying/
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
 
