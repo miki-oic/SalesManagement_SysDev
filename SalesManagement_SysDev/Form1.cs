96e4f43a6edd237ac6ea270ee6d2f97939829ac4
miki <miki@oic.jp>
2024-11-08 11:09:42 +0900

    データ一覧表示の実装

---
+++ SalesManagement_SysDev/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagement_SysDev
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            Initialize();
+
+        }
+
+        private void Initialize()
+        {
+
+            SalesManagementContext context = new SalesManagementContext();
+
+            IDbContextTransaction dbContextTransaction = context.Database.BeginTransaction();
+            
+            dataGridView1.DataSource = context.MMakers.ToList();
+
+        }
+
+    }
+
+}
